755d50b9e0d6b7970fd38b358f21d492f2bfc425
kai <kai@simbo.com.tw>
2020-12-20 15:15:20 +0800

    add bootstrapVue

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import { routes } from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const router = new VueRouter({
   routes,
