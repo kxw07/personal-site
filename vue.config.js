452e28ffb352738190459944a2ddcd991f601900
kai <kai@simbo.com.tw>
2020-12-27 17:45:10 +0800

    modify title and icon.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Kai\'s Blog'
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/kxw07.github.io/'
     : '/'
