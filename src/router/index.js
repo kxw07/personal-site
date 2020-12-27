eedf69acdb303e658d753e6b9650696c6908ff88
kai <kai@simbo.com.tw>
2020-12-27 17:09:15 +0800

    remove file

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,8 @@
-import MyDescription from '@/components/MyDescription'
 import WhoAmI from "@/components/WhoAmI";
 
 export const routes = [
   {
     path: '/',
     component: WhoAmI
-  },
-  {
-    path: '/myDescription',
-    name: 'MyDescription',
-    component: MyDescription
   }
 ]
